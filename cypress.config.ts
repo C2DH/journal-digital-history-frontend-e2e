58c5b6e793cf4a36f6fe5a64da36defe88d87e47
salaun.marion <marion.salaun@uni.lu>
2025-11-13 16:53:20 +0100

    (upgrade) package.json update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "cypress";
-import 'dotenv/config';
+import "dotenv/config";
 
 export default defineConfig({
+  // projectId: process.env.CYPRESS_PROJECT_ID,
   e2e: {
-    baseUrl: process.env.BASE_URL ??'https://journalofdigitalhistory.org/en',
+    baseUrl: process.env.BASE_URL ?? "https://journalofdigitalhistory.org/en",
     experimentalStudio: true,
     chromeWebSecurity: false,
+    experimentalPromptCommand: true,
   },
   env: {
-    ... process.env,
+    ...process.env,
   },
   retries: 2,
 });
